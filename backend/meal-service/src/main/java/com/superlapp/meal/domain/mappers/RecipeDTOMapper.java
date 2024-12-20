e86f59606bf59787bb91740354293054134fda02
OliverKeefe <oliverkeefe@tutanota.com>
2024-12-20 19:27:20 +0000

    feat: Implemented RecipeDTOMapper class

---
+++ backend/meal-service/src/main/java/com/superlapp/meal/domain/mappers/RecipeDTOMapper.java
@@ -0,0 +1,10 @@
+package com.superlapp.meal.domain.mappers;
+
+import com.superlapp.core.BaseDTOMapper;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class RecipeDTOMapper extends BaseDTOMapper {
+
+
+}
