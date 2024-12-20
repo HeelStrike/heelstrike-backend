6243c214dbd4be4119afd67dbbd6f3f0123ca587
OliverKeefe <oliverkeefe@tutanota.com>
2024-12-20 15:25:00 +0000

    feat: Implemented AllergenDTO class

---
+++ backend/meal-service/src/main/java/com/superlapp/meal/domain/dto/AllergenDTO.java
@@ -0,0 +1,9 @@
+package com.superlapp.meal.domain.dto;
+
+import com.superlapp.core.BaseDTO;
+
+public class AllergenDTO extends BaseDTO {
+    public AllergenDTO(int id, String name) {
+        super(id, name);
+    }
+}
