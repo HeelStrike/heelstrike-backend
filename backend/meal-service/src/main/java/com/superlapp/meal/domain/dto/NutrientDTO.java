a6afaaa233b6ef5623c90dc9557ec884c37fc7ab
OliverKeefe <oliverkeefe@tutanota.com>
2024-12-20 15:23:47 +0000

    feat: Implemented NutrientDTO class

---
+++ backend/meal-service/src/main/java/com/superlapp/meal/domain/dto/NutrientDTO.java
@@ -0,0 +1,11 @@
+package com.superlapp.meal.domain.dto;
+
+import com.superlapp.core.BaseDTO;
+
+public class NutrientDTO extends BaseDTO {
+
+    public NutrientDTO(int id, String name) {
+        super(id, name);
+    }
+
+}
