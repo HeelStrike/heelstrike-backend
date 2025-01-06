f67f398d66ce096adc396ce5446a85f89318b48f
OliverKeefe <oliverkeefe@tutanota.com>
2025-01-06 15:57:45 +0000

    feat: Added TokenProducer
    	- Implemented produceUserId, produceUsername and produceRoles
    	  methods.
    	- TokenProducer takes UserEntity class as a parameter, returns
    	  data required for the 'production' of a JWT token.
    Part of the application layer for the auth service.

--- backend/auth-service/src/main/java/com/superlapp/auth/application/producers/TokenProducer.java
+++ backend/auth-service/src/main/java/com/superlapp/auth/application/producers/TokenProducer.java
@@ -1,30 +1,47 @@
-package com.superlapp.meal.application;
+package com.superlapp.auth.application.producers;
+
+import com.superlapp.auth.domain.entity.RoleEntity;
+import com.superlapp.auth.domain.entity.UserEntity;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
+import jakarta.persistence.Entity;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
 @ApplicationScoped
-public class TokenProducer<UserEntity> {
+public class TokenProducer {
 
     @Inject
     UserEntity userEntity;
 
+    public TokenProducer(UserEntity userEntity) {
+        this.userEntity = userEntity;
+    }
+
     @Produces
     public UUID produceUserId() {
-        return userEntity.getId();
+        return userEntity.getUuid();
     }
 
     @Produces
-    public String username() {
-        return userEntity.getUsername();
+    public String produceUsername() {
+        return userEntity.getName();
     }
 
     @Produces
-    public Set<String> roles() {
-        return userEntity.getRoles();
+    public Set<String> produceRoles() {
+        Set<String> roleSet = new java.util.HashSet<>(Collections.emptySet());
+        List<RoleEntity> roleList = userEntity.getRoles();
+
+        for (RoleEntity roleEntity : roleList) {
+            roleSet.add(roleEntity.getName());
+        }
+
+        return roleSet;
     }
 }
