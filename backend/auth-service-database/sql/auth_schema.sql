ec1ca49fc945d6cc5069e13ffe2b7120917b603b
OliverKeefe <oliverkeefe@tutanota.com>
2025-01-16 22:59:06 +0000

    feat: Added auth_schema.sql
    	- Added extention uuid-ossp
    		- This adds support in db for UUID types
    	- Created role_permissions table
    		- id, role_name
    	- Created users table
    		- id, user_uuid, username, password_hash,
    		  role_permissions_id FK
    	- Created user_roles table
    		- user_id FK, role_id FK (user_id, role_id) Composite
    		  Key PK

--- backend/auth-service-database/sql/auth_schema.sql
+++ backend/auth-service-database/sql/auth_schema.sql
@@ -4,14 +4,24 @@ CREATE DATABASE auth_db;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS role (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(10)
+CREATE TABLE IF NOT EXISTS role_permissions (
+    id BIGSERIAL PRIMARY KEY,
+    role_name VARCHAR(10)
 );
 
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    username VARCHAR(20),
-    role_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id)
+    id BIGSERIAL PRIMARY KEY,
+    user_uuid UUID DEFAULT uuid_generate_v4() UNIQUE,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    role_permissions_id BIGINT,
+    FOREIGN KEY (role_permissions_id) REFERENCES role_permissions(id)
+);
+
+CREATE TABLE IF NOT EXISTS user_roles (
+    user_id BIGINT,
+    role_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (role_id) REFERENCES role_permissions(id),
+    PRIMARY KEY (user_id, role_id)
 );
