508e45a153a4caa5fbe2eae3e90024aba0781159
OliverKeefe <oliverkeefe@tutanota.com>
2025-01-16 22:32:55 +0000

    feat: Implemented RoleEntity class
    	- Implemented getters / setters for fields:
    		id, name
    	- Added ManyToMany annotation for List<UserEntity> users mapped
    	  by userRoles
    
    	- Added jakarta.persistance annotations.

--- backend/auth-service/src/main/java/com/superlapp/auth/domain/entity/RoleEntity.java
+++ backend/auth-service/src/main/java/com/superlapp/auth/domain/entity/RoleEntity.java
@@ -3,33 +3,36 @@ package com.superlapp.auth.domain.entity;
 import com.superlapp.core.BaseEntity;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
-@Table(name = "role")
-public class RoleEntity extends BaseEntity {
+@Table(name = "role_permissions")
+public class RoleEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
 
     @Column(name = "role_name")
     private String name;
 
-    @Override
-    public int getId() {
+    @ManyToMany(mappedBy = "userRoles")
+    private List<UserEntity> users = new ArrayList<>();
+
+    public long getId() {
         return this.id;
     }
 
-    @Override
-    public void setId(int newId) {
+    public void setId(long newId) {
         this.id = newId;
     }
 
-    @Override
     public String getName() {
         return this.name;
     }
 
-    @Override
     public void setName(String newName) {
         this.name = newName;
     }
