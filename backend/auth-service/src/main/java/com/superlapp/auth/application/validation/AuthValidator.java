20e4c59359dd98401f0aa8ab764d8901dc4ff98d
OliverKeefe <oliverkeefe@tutanota.com>
2025-01-16 22:49:28 +0000

    refactor: Modified AuthValidator to  accept UserDTO as parameter
    	- Implemented validateUser method to ascertain if user with
    	  said UUID exists
    	- Takes UserDTO as parameter rather than a query supplied UUID
    	  string.

--- backend/auth-service/src/main/java/com/superlapp/auth/application/validation/AuthValidator.java
+++ backend/auth-service/src/main/java/com/superlapp/auth/application/validation/AuthValidator.java
@@ -1,4 +1,30 @@
 package com.superlapp.auth.application.validation;
 
+import com.superlapp.auth.domain.dto.UserDTO;
+import com.superlapp.auth.domain.repository.UserRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import java.util.Set;
+import java.util.UUID;
+
+@ApplicationScoped
 public class AuthValidator {
+    private UUID userUuid;
+    private String username;
+    private Set<String> roles;
+
+    @Inject
+    UserRepository userRepository;
+
+    public AuthValidator(UserDTO userDTO) {
+        this.userUuid = userDTO.getUuid();
+        this.username = userDTO.getName();
+        this.roles = userDTO.getRoles();
+    }
+
+    public boolean validateUser() {
+        return userUuid == userRepository.findUserEntity(userUuid).getUuid();
+    }
+
 }
