021d301f6adc65f4d7a98f69e185bfdb5934e768
OliverKeefe <oliverkeefe@tutanota.com>
2025-01-16 22:51:29 +0000

    feat: Added exception handling, modified to accept UserDTO as parameter

--- backend/auth-service/src/main/java/com/superlapp/auth/application/service/TokenService.java
+++ backend/auth-service/src/main/java/com/superlapp/auth/application/service/TokenService.java
@@ -1,53 +1,68 @@
 package com.superlapp.auth.application.service;
 
 import com.superlapp.auth.application.producers.TokenProducer;
-import io.vertx.ext.auth.impl.jose.JWT;
+import com.superlapp.auth.application.validation.AuthValidator;
+import com.superlapp.auth.domain.repository.UserRepository;
 
+import com.superlapp.auth.domain.dto.UserDTO;
 import com.superlapp.auth.domain.entity.UserEntity;
 
-import java.util.List;
+
 import java.util.UUID;
-import io.quarkus.security.User;
-import io.quarkus.security.identity.SecurityIdentity;
+
 import io.smallrye.jwt.build.Jwt;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import jdk.jshell.spi.ExecutionControl;
 
 import java.time.Duration;
-import java.util.HashSet;
 import java.util.Set;
 
 @ApplicationScoped
 public class TokenService {
 
-    private UUID userId;
+    private UUID userUuid;
     private String username;
     private Set<String> roles;
 
     @Inject
-    UserEntity userEntity;
+    UserRepository userRepository;
+
+    @Inject
+    AuthValidator authValidator;
 
     @Inject
     TokenProducer tokenProducer;
 
-    public TokenService(UserEntity userEntity) {
-        this.userId = tokenProducer.produceUserId();
-        this.username = tokenProducer.produceUsername();
-        this.roles = tokenProducer.produceRoles();
+    @Inject
+    UserDTO userDTO;
+
+    public TokenService() {}
+
+    public String generate(UserDTO userDTO) {
+        if (authValidator.validateUser()) {
+            try {
+
+                UserEntity userEntity = userRepository.findUserEntity(
+                        userDTO.getUuid()
+                );
+
+                userUuid = tokenProducer.produceUserId(userEntity);
+                username = tokenProducer.produceUsername(userEntity);
+                roles = tokenProducer.produceRoles(userEntity);
+
+                return Jwt
+                        .issuer("https://heelstrike.app")
+                        .subject(username)
+                        .groups(roles)
+                        .expiresIn(Duration.ofHours(2))
+                        .sign();
+
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to generate JWT, " + e);
+            }
+        }
+
+        return ("User not found, unable to produce JWT.");
     }
 
-    public String generate() {
-        return Jwt
-                .issuer("https://heelstrike.app")
-                .subject(username)
-                .groups(roles)
-                .expiresIn(Duration.ofHours(2))
-                .sign();
-    }
 }
