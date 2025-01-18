29eaa7c694d9b993c2dd02980c51df6ca6e853f0
OliverKeefe <oliverkeefe@tutanota.com>
2025-01-18 14:29:27 +0000

    feat: Added findByUsername, findByUuid methods in UserRepository
    	- Implemented findByUuid method
    	- Implemented findByUsername method
    
    Swithced to Panache rather than using JPA and BaseRepository in core
    package. Simplifies JWT issuing system massively.

--- backend/auth-service/src/main/java/com/superlapp/auth/domain/repository/UserRepository.java
+++ backend/auth-service/src/main/java/com/superlapp/auth/domain/repository/UserRepository.java
@@ -2,19 +2,25 @@ package com.superlapp.auth.domain.repository;
 
 import com.superlapp.auth.domain.entity.UserEntity;
 
+import com.superlapp.core.BaseRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.inject.Inject;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @ApplicationScoped
-public class UserRepository implements PanacheRepositoryBase<UserEntity, Long>{
+//public class UserRepository implements PanacheRepositoryBase<UserEntity, Long>{
     //TODO: Refactor to return an Optional data type. and also not to shit the bed when findUserEntity returns null.
+public class UserRepository implements PanacheRepositoryBase<UserEntity, UUID> {
 
-    public UserEntity findUserEntity(UUID userUuid) {
-        System.out.println(userUuid);
-        UserEntity userEntity = find("user_uuid", userUuid).firstResult();
-        return userEntity;
+    public Optional<UserEntity> findByUuid(UUID uuid) {
+        return find("user_uuid", uuid).firstResultOptional();
+    }
+
+    public Optional<UserEntity> findByUsername(String username) {
+        return find("username", username).firstResultOptional();
     }
 }
