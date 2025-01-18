e4375d0a3e459563454b77966e10bf0632430df8
OliverKeefe <oliverkeefe@tutanota.com>
2025-01-18 14:48:19 +0000

    refactor: Modified TokenProducer to produce JWT data using Optional type
    Now accepts Optional<UserEntity> as param, maps UserEntity data to
    relevant datatype and returns it, or, throws IllegalArgumentException.

--- backend/auth-service/src/main/java/com/superlapp/auth/application/producers/TokenProducer.java
+++ backend/auth-service/src/main/java/com/superlapp/auth/application/producers/TokenProducer.java
@@ -4,34 +4,26 @@ import com.superlapp.auth.domain.entity.RoleEntity;
 import com.superlapp.auth.domain.entity.UserEntity;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Produces;
-import jakarta.inject.Inject;
-import jakarta.persistence.Entity;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @ApplicationScoped
 public class TokenProducer {
 
-    public UUID produceUserId(UserEntity userEntity) {
-        return userEntity.getUuid();
+    public UUID produceUserId(Optional<UserEntity> userEntityOptional) {
+        return userEntityOptional.map(UserEntity::getUuid)
+                .orElseThrow(() -> new IllegalArgumentException("Could not get UUID, UserEntity not present."));
     }
 
-    public String produceUsername(UserEntity userEntity) {
-        return userEntity.getName();
+    public String produceUsername(Optional<UserEntity> userEntityOptional) {
+        return userEntityOptional.map(UserEntity::getUsername)
+                .orElseThrow(() -> new IllegalArgumentException("Could not get username, UserEntity not present."));
     }
 
-    public Set<String> produceRoles(UserEntity userEntity) {
-        Set<String> roleSet = new java.util.HashSet<>(Collections.emptySet());
-        List<RoleEntity> roleList = userEntity.getRoles();
+    public RoleEntity produceRoles(Optional<UserEntity> userEntityOptional) {
 
-        for (RoleEntity roleEntity : roleList) {
-            roleSet.add(roleEntity.getName());
-        }
+        return userEntityOptional.map(UserEntity::getRole)
+                .orElseThrow(() -> new IllegalArgumentException("Could not get user role, UserEntity not present."));
 
-        return roleSet;
     }
 }
