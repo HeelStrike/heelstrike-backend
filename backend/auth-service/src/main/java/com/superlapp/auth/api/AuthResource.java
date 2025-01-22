9a94dd7d4e31fa2c02fa0e5ac53952db7e519de5
OliverKeefe <oliverkeefe@tutanota.com>
2025-01-22 19:14:31 +0000

    feat: Added create-user endpoint
    	- Implemented endpoint to handle user creation with relevant
    	  query parameters including username, password, primary email
    	  and mobile number.
    	- Included validation to check if user already exists (Need to
    	  also add check for primary email address (potentially
    	  secondary also, haven't decided if needs to be unique yet.
    	- Preserved existing DTO logic for data populationi and
    	  invocation of service class methods.

--- backend/auth-service/src/main/java/com/superlapp/auth/api/AuthResource.java
+++ backend/auth-service/src/main/java/com/superlapp/auth/api/AuthResource.java
@@ -1,6 +1,7 @@
 package com.superlapp.auth.api;
 
 import com.superlapp.auth.application.service.TokenService;
+import com.superlapp.auth.application.service.UserCreationService;
 import com.superlapp.auth.application.validation.AuthValidator;
 import com.superlapp.auth.domain.dto.UserDTO;
 import jakarta.inject.Inject;
@@ -23,11 +24,15 @@ public class AuthResource {
     @Inject
     AuthValidator authValidator;
 
+    @Inject
+    UserCreationService userCreationService;
+
     @POST
     @Path("/token")
     @Consumes("application/json")
     @Produces(MediaType.TEXT_PLAIN)
-    public Response giveToken(@QueryParam("username") String username) {
+    public Response giveToken(@QueryParam("username") String username,
+                              @QueryParam("password") String password) {
 
         if (!authValidator.validateUser(username)) {
             return Response.status(Response.Status.NOT_FOUND)
@@ -36,9 +41,38 @@ public class AuthResource {
         }
 
         userDTO.setUsername(username);
+        userDTO.setPassword(password);
 
         String token = tokenService.generate(userDTO);
 
         return Response.ok(token).build();
+    }
+
+    //TODO: Add query field for setting RBAC permissions.
+    @POST
+    @Path("/create-user")
+    @Consumes("application/json")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response createUser(@QueryParam("newUsername") String newUsername,
+                               @QueryParam("newPassword") String newPassword,
+                               @QueryParam("primaryEmail") String primaryEmail,
+                               @QueryParam("mobile") long mobile
+                               ) {
+
+        if (authValidator.validateUser(newUsername)) {
+            return Response.status(Response.Status.CONFLICT)
+                    .entity("User: " + newUsername + ", already exists!")
+                    .build();
+        }
+
+        userDTO.setUsername(newUsername);
+        userDTO.setPassword(newPassword);
+        userDTO.setPrimaryEmail(primaryEmail);
+        userDTO.setMobile(mobile);
+        //userDTO.setRole(1);
+
+        userCreationService.createUser(userDTO);
+
+        return Response.ok(userDTO).build();
     }
 }
