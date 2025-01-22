1c7dd33e22867db6d7ed8e91b415834adaaefb32
OliverKeefe <oliverkeefe@tutanota.com>
2025-01-22 19:20:17 +0000

    feat: UserCreationService class
    	- Added `createUser` method to handle creation of new user
    	  entities in db.
    	- Hashes and verifies passwords by invoking `PasswordService`
    	  class methods.
    	- Sets UserEntity fields provided from `UserDTO` Data Transfer
    	  Object.
    	- Fetches user role from db and if not specified, will assign
    	  standard user RBAC.
    	- Persists new user in db, added exception handling.
    	- Jakarta transactional annotation, marks transactional
    	  integrity for user creation process.

---
+++ backend/auth-service/src/main/java/com/superlapp/auth/application/service/UserCreationService.java
@@ -0,0 +1,77 @@
+package com.superlapp.auth.application.service;
+
+import com.superlapp.auth.domain.dto.UserDTO;
+import com.superlapp.auth.domain.entity.RoleEntity;
+import com.superlapp.auth.domain.entity.UserEntity;
+import com.superlapp.auth.domain.repository.RoleRepository;
+import com.superlapp.auth.domain.repository.UserRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+import javax.management.relation.Role;
+
+@ApplicationScoped
+public class UserCreationService {
+
+    @Inject
+    UserRepository userRepository;
+
+    @Inject
+    RoleRepository roleRepository;
+
+    @Inject
+    PasswordService passwordService;
+
+    public UserCreationService() {
+    }
+
+    @Transactional
+    public void createUser(UserDTO userDTO) {
+        UserEntity userEntity = new UserEntity();
+
+        try {
+            String passwordHash = passwordService.hashPassword(userDTO.getPassword());
+
+            if (!passwordService.verifyPassword(passwordHash, userDTO.getPassword())) {
+                throw new RuntimeException("UserDTO supplied password and argon2 password hash do not match.");
+            }
+
+            userDTO.setPasswordHash(passwordHash);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to hash password, " + e.getMessage(), e);
+        }
+
+        userEntity.setUsername(userDTO.getUsername());
+        userEntity.setPasswordHash(userDTO.getPasswordHash());
+        userEntity.setMobile(userDTO.getMobile());
+        userEntity.setPrimaryEmail(userDTO.getPrimaryEmail());
+
+        if (userDTO.getSecondaryEmail() != null) {
+            userEntity.setSecondaryEmail(userDTO.getSecondaryEmail());
+        }
+
+        RoleEntity roleEntity;
+        if (userDTO.getRoleId() != null) {
+            roleEntity = roleRepository.findRoleById(userDTO.getRoleId())
+                    .orElseThrow(() -> new RuntimeException("Role with ID " + userDTO.getRoleId() + " not found."));
+        } else {
+            roleEntity = roleRepository.findRoleById(1L)
+                    .orElseThrow(() -> new RuntimeException("Default role not found in database. Expecting | id: 1 | role_name: 'user' |"));
+        }
+
+        userEntity.setRole(roleEntity);
+
+        try {
+            userRepository.persistUserEntity(userEntity);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Could not create new user, " + e.getMessage(), e);
+        }
+    }
+
+    public void updateUserRole(UserDTO userDTO) {
+        UserEntity userEntity = userRepository.findById(userDTO.getUuid());
+    }
+}
