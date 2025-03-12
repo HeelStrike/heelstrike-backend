42f94e86ae76a23785be827875f18aa7c78665b9
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-12 15:57:04 +0000

    refactor(meal-service): Re-write Recipe Database Schema
    Implemented following tables:
    	- recipe
    	- diet
    	- recipe_dietary_suitability
    	- nutrient
    	- micro_ingredient
    	- nutrient_micro_ingredient
    	- macro_ingredient
    	- micro_ingredient_macro_ingredient
    	- allergen

--- backend/meal-service-database/sql/recipe_schema.sql
+++ backend/meal-service-database/sql/recipe_schema.sql
@@ -2,115 +2,73 @@ CREATE DATABASE recipe_db;
 
 \c recipe_db;
 
-CREATE TABLE IF NOT EXISTS allergen (
-    id SERIAL PRIMARY KEY,
-    allergen_name VARCHAR(16),
-    category VARCHAR(5)
-);
-
--- Prevent duplicate Allergen names
--- ALTER TABLE allergen ADD CONSTRAINT unique_name_category UNIQUE (allergen_name);
-
-
-CREATE TABLE IF NOT EXISTS macro_ingredient (
-    id SERIAL PRIMARY KEY,
-    macro_ingredient_name VARCHAR(64),
-    allergen_id INT,
-    FOREIGN KEY (allergen_id) REFERENCES allergen(id)
-);
-
-CREATE TABLE IF NOT EXISTS micro_ingredient (
-    id SERIAL PRIMARY KEY,
-    micro_ingredient_name VARCHAR(64),
-    allergen_id INT,
-    FOREIGN KEY (allergen_id) REFERENCES allergen(id)
-);
-
-CREATE TABLE IF NOT EXISTS diet (
-    id SERIAL PRIMARY KEY,
-    diet_name VARCHAR(16)
-);
-
-CREATE TABLE IF NOT EXISTS nutrient (
-    id SERIAL PRIMARY KEY,
-    nutrient_name VARCHAR (64),
-    measurement VARCHAR (5)
-);
-
--- ALTER TABLE nutrient ADD CONSTRAINT nutrient_name UNIQUE (nutrient_name)
-
-CREATE TABLE IF NOT EXISTS recipe_image (
-    id SERIAL PRIMARY KEY,
-    image_url TEXT
-);
+/*
+    Schema for Recipe Database.
+    Stores data for recipe information, used in the creation of Meal Plans.
+*/
 
 CREATE TABLE IF NOT EXISTS recipe (
-    id SERIAL PRIMARY KEY,
-    recipe_name VARCHAR(128),
+    id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
     cooking_instructions TEXT,
-    recipe_image_id INT,
-    CONSTRAINT fk_recipe_image_id FOREIGN KEY (recipe_image_id) REFERENCES recipe_image(id) ON DELETE SET NULL
+    preparation_time INTERVAL,
+    cooking_time INTERVAL,
+    serves INT CHECK (serves > 0),
+    macro_ingredient_id BIGINT REFERENCES macro_ingredient(id) ON DELETE SET NULL,
+    dietary_suitability BIGINT REFERENCES diet(id) ON DELETE SET NULL
 );
+COMMENT ON TABLE recipe IS 'Table for storing recipe data.';
 
--- Many-To-Many Intermediary Table: Recipe to Allergen
-CREATE TABLE IF NOT EXISTS recipe_allergen (
-    recipe_id INT,
-    allergen_id INT,
-    PRIMARY KEY (recipe_id, allergen_id),
-    FOREIGN KEY (recipe_id) REFERENCES recipe(id),
-    FOREIGN KEY (allergen_id) REFERENCES allergen(id)
+CREATE TABLE IF NOT EXISTS diet (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(26) NOT NULL UNIQUE
 );
+COMMENT ON TABLE diet IS 'Table for storing diet names, e.g. Halal, Vegan, Keto etc...';
 
--- Many-To-Many Intermediary Table: Recipe to Nutrient
-CREATE TABLE IF NOT EXISTS recipe_nutrient (
-    recipe_id INT NOT NULL,
-    nutrient_id INT NOT NULL,
-    PRIMARY KEY (recipe_id, nutrient_id),
-    FOREIGN KEY (recipe_id) REFERENCES recipe(id) ON DELETE CASCADE,
-    FOREIGN KEY (nutrient_id) REFERENCES nutrient(id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS recipe_dietary_suitability (
+    id BIGSERIAL PRIMARY KEY,
+    recipe_id BIGINT NOT NULL REFERENCES recipe(id) ON DELETE CASCADE,
+    diet_id BIGINT NOT NULL REFERENCES diet(id) ON DELETE CASCADE
 );
+COMMENT ON TABLE recipe_dietary_suitability IS 'Joins recipe and diet table to provide Many-To-Many relationship for determining dietary suitability of recipe.';
 
--- Many-To-Many Intermediary Table: Recipe to MacroIngredient
-CREATE TABLE IF NOT EXISTS recipe_macro_ingredient(
-    recipe_id INT,
-    macro_ingredient_id INT,
-    PRIMARY KEY (recipe_id, macro_ingredient_id),
-    FOREIGN KEY (recipe_id) REFERENCES recipe(id),
-    FOREIGN KEY (macro_ingredient_id) REFERENCES macro_ingredient(id)
+CREATE TABLE IF NOT EXISTS nutrient (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL UNIQUE
 );
+COMMENT ON TABLE nutrient IS 'Table for storing nutrients.';
 
--- Many-To-Many Intermediary Table: Recipe to MicroIngredient
-CREATE TABLE IF NOT EXISTS recipe_micro_ingredient (
-    recipe_id INT,
-    micro_ingredient_id INT,
-    PRIMARY KEY (recipe_id, micro_ingredient_id),
-    FOREIGN KEY (recipe_id) REFERENCES recipe(id),
-    FOREIGN KEY (micro_ingredient_id) REFERENCES micro_ingredient(id)
+CREATE TABLE IF NOT EXISTS micro_ingredient (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(64) NOT NULL UNIQUE
 );
+COMMENT ON TABLE micro_ingredient IS 'Table for storing micro_ingredients. E.g. Garlic, Tomato, Basil.';
 
--- Many-To-Many Intermediary Table: Recipe to Diet
-CREATE TABLE IF NOT EXISTS dietary_suitability ( -- e.g. Halal, Vegan, Kosher, Keto etc...;
-    recipe_id INT NOT NULL,
-    diet_id INT NOT NULL,
-    PRIMARY KEY (recipe_id, diet_id),
-    FOREIGN KEY (recipe_id) REFERENCES recipe(id),
-    FOREIGN KEY (diet_id) REFERENCES diet(id)
+CREATE TABLE IF NOT EXISTS nutrient_micro_ingredient (
+    id BIGSERIAL PRIMARY KEY,
+    nutrient_id BIGINT NOT NULL REFERENCES nutrient(id) ON DELETE CASCADE,
+    micro_ingredient_id BIGINT NOT NULL REFERENCES micro_ingredient(id) ON DELETE CASCADE,
+    UNIQUE (nutrient_id, micro_ingredient_id)
 );
+COMMENT ON TABLE nutrient_micro_ingredient IS 'Joins nutrient and micro_ingredient tables for Many-To-Many relationship.';
 
--- Many-To-Many Intermediary Table: MicroIngredient to Allergen
-CREATE TABLE IF NOT EXISTS micro_ingredient_allergen(
-    micro_ingredient_id INT,
-    allergen_id INT,
-    PRIMARY KEY (micro_ingredient_id, allergen_id),
-    FOREIGN KEY (micro_ingredient_id) REFERENCES micro_ingredient(id),
-    FOREIGN KEY (allergen_id) REFERENCES allergen(id)
+CREATE TABLE IF NOT EXISTS macro_ingredient (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(64) NOT NULL
 );
+COMMENT ON TABLE macro_ingredient IS 'Table for storing macro_ingredients. E.g. Tomato Sauce.';
 
--- Many-To-Many Intermediary Table: MacroIngredient to Allergen
-CREATE TABLE IF NOT EXISTS macro_ingredient_allergen(
-    macro_ingredient_id INT,
-    allergen_id INT,
-    PRIMARY KEY (macro_ingredient_id, allergen_id),
-    FOREIGN KEY (macro_ingredient_id) REFERENCES macro_ingredient(id),
-    FOREIGN KEY (allergen_id) REFERENCES allergen(id)
+CREATE TABLE IF NOT EXISTS micro_ingredient_macro_ingredient (
+    id BIGSERIAL PRIMARY KEY,
+    macro_ingredient_id BIGINT NOT NULL REFERENCES macro_ingredient(id) ON DELETE CASCADE,
+    micro_ingredient_id BIGINT NOT NULL REFERENCES micro_ingredient(id) ON DELETE CASCADE,
+    UNIQUE (macro_ingredient_id, micro_ingredient_id)
 );
+COMMENT ON TABLE micro_ingredient_macro_ingredient IS 'Joins tables micro_ingredient and macro_ingredient for Many-To-Many relationship.';
+
+CREATE TABLE IF NOT EXISTS allergen (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(64) NOT NULL UNIQUE
+);
+COMMENT ON TABLE allergen IS 'Table for storing allergens.';
