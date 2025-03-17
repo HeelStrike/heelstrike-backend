e818e3dc5ed0de1509ef972997fe0c4ff9376850
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-17 18:08:12 +0000

    feat(meal-service): Add MacroIngredientDTO

--- backend/meal-service/src/main/java/com/heelstrike/meal/domain/dto/MacroIngredientDTO.java
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/dto/MacroIngredientDTO.java
@@ -1,4 +1,22 @@
 package com.heelstrike.meal.domain.dto;
 
 public class MacroIngredientDTO {
+    private long id;
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
