3c17808e571c62d0f0aa71e6dc6d3e97cc7acf9d
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-17 17:40:18 +0000

    feat(meal-service): Add AllergenMicroIngredientEntity
    Entity class for joining allergen table and micro_ingredient table.

---
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/AllergenMicroIngredientEntity.java
@@ -0,0 +1,48 @@
+package com.heelstrike.meal.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "allergen_micro_ingredient")
+public class AllergenMicroIngredientEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(
+            name = "id",
+            updatable = false,
+            nullable = false
+    )
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "allergen_id", nullable = false)
+    private AllergenEntity allergen_id;
+
+    @ManyToOne
+    @JoinColumn(name = "micro_ingredient_id", nullable = false)
+    private MicroIngredientEntity microIngredient;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public MicroIngredientEntity getMicroIngredient() {
+        return microIngredient;
+    }
+
+    public void setMicroIngredient(MicroIngredientEntity microIngredient) {
+        this.microIngredient = microIngredient;
+    }
+
+    public AllergenEntity getAllergen_id() {
+        return allergen_id;
+    }
+
+    public void setAllergen_id(AllergenEntity allergen_id) {
+        this.allergen_id = allergen_id;
+    }
+}
