7a5b179d646af3f422cb255e2f979b000963b70d
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-17 12:18:40 +0000

    feat(meal-service): Add MicroIngredientEntity

---
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/MicroIngredientEntity.java
@@ -0,0 +1,27 @@
+package com.heelstrike.meal.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "micro_ingredient")
+public class MicroIngredientEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
