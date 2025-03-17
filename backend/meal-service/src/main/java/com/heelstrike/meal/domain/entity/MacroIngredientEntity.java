8ed081cbf22c11317c6cdcbd8dcbc9413f6869a2
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-17 12:16:29 +0000

    feat(meal-service): Add MacroIngredientEntity

--- backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/MacroIngredientEntity.java
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/MacroIngredientEntity.java
@@ -1,4 +1,29 @@
 package com.heelstrike.meal.domain.entity;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "macro_ingredient")
 public class MacroIngredientEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
