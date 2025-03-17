78944a6c5f807c5be8e66dd570cc28e479a476f5
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-17 12:15:05 +0000

    feat(meal-service): Add AllergenEntity

---
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/AllergenEntity.java
@@ -0,0 +1,26 @@
+package com.heelstrike.meal.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "allergen")
+public class AllergenEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
