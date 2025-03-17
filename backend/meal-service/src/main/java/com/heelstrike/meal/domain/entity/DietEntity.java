fa00e32c93c23d25e41dc008bc470a716d3b33f8
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-17 12:15:26 +0000

    feat(meal-service): Add DietEntity

--- backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/DietEntity.java
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/DietEntity.java
@@ -1,4 +1,27 @@
 package com.heelstrike.meal.domain.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "diet")
 public class DietEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
