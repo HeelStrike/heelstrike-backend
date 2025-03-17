b92d4ce57ae668ba615c8d7423f8750a8a259066
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-17 17:55:47 +0000

    fix(meal-service): Fix MicroIngredientEntity

--- backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/MicroIngredientEntity.java
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/MicroIngredientEntity.java
@@ -2,16 +2,29 @@ package com.heelstrike.meal.domain.entity;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "micro_ingredient")
 public class MicroIngredientEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", updatable = false, nullable = false)
+    @Column(
+            name = "id",
+            updatable = false,
+            nullable = false
+    )
     private long id;
 
     @Column(name = "name")
     private String name;
+
+    @OneToMany(
+            mappedBy = "macro_ingredient",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<MicroIngredientMacroIngredientEntity> macroIngredientLinks;
 
     public long getId() {
         return this.id;
@@ -23,5 +36,13 @@ public class MicroIngredientEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<MicroIngredientMacroIngredientEntity> getMacroIngredientLinks() {
+        return this.macroIngredientLinks;
+    }
+
+    public void setMacroIngredientLinks(List<MicroIngredientMacroIngredientEntity> macroIngredientLinks) {
+        this.macroIngredientLinks = macroIngredientLinks;
     }
 }
