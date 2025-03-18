58cc04eb748cbbdc1a86f74e85dbb6b84a255f5f
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-18 13:48:28 +0000

    feat(meal-service): Add NutrientDTO

---
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/dto/NutrientDTO.java
@@ -0,0 +1,22 @@
+package com.heelstrike.meal.domain.dto;
+
+public class NutrientDTO {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
