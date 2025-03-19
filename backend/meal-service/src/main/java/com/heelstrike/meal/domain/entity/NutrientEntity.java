c445345e7abdf681bb3f88afb5dc7f6abce0f21e
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-19 16:39:40 +0000

    feat(meal-service): Add NutrientEntity

---
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/NutrientEntity.java
@@ -0,0 +1,54 @@
+package com.heelstrike.meal.domain.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "nutrient")
+public class NutrientEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(
+            name = "id",
+            updatable = false,
+            nullable = false
+    )
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "nutrient_micro_ingredient",
+            joinColumns = @JoinColumn(name = "nutrient_id"),
+            inverseJoinColumns = @JoinColumn(name = "micro_ingredient_id")
+    )
+    private Set<MicroIngredientEntity> microIngredients;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<MicroIngredientEntity> getMicroIngredients() {
+        return this.microIngredients;
+    }
+
+    public void setMicroIngredients(Set<MicroIngredientEntity> microIngredients) {
+        this.microIngredients = microIngredients;
+    }
+}
