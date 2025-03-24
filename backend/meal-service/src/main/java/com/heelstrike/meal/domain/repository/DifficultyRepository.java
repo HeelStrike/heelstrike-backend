ff096c9530f3201df18314792c45babdfc76b2d8
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-24 20:03:29 +0000

    feat(meal-service): Add DifficultyRepository

---
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/repository/DifficultyRepository.java
@@ -0,0 +1,7 @@
+package com.heelstrike.meal.domain.repository;
+
+import com.heelstrike.meal.domain.entity.DifficultyEntity;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class DifficultyRepository implements PanacheRepository<DifficultyEntity> {
+}
