3efce3b0f5ac754b3fe07326046aeda83179509d
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-24 21:30:32 +0000

    refactor(meal-service): RecipeRepository
    - Decomposed business logic out into service class.
    - Added findByDynamicQuery method
    - Added findByTitle method

--- backend/meal-service/src/main/java/com/heelstrike/meal/domain/repository/RecipeRepository.java
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/repository/RecipeRepository.java
@@ -23,51 +23,11 @@ public class RecipeRepository implements PanacheRepository<RecipeEntity> {
         return find("id", id).firstResultOptional();
     }
 
-    public List<RecipeEntity> findRecipesByRequirements(RecipeRequirementsDTO requirements) {
-        String query = "SELECT DISTINCT r FROM RecipeEntity r " +
-                "LEFT JOIN FETCH r.macroIngredients m " +
-                "LEFT JOIN FETCH m.microIngredients mi " +
-                "LEFT JOIN FETCH mi.nutrients n " +
-                "LEFT JOIN FETCH r.dietarySuitability dts " +
-                "LEFT JOIN FETCH mi.allergens a " +
-                "WHERE 1=1";
+    public Optional<RecipeEntity> findByTitle(String title) {
+        return find("title", title).firstResultOptional();
+    }
 
-        Map<String, Object> params = new HashMap<>();
-
-        if (requirements.getAllergensToAvoid() != null && !requirements.getAllergensToAvoid().isEmpty()) {
-            query += "AND COALESCE(a.name, '') NOT IN (:allergens) ";
-            params.put("allergens",
-                    requirements.getAllergensToAvoid()
-                            .stream()
-                            .map(AllergenDTO::getName)
-                            .toList()
-            );
-        }
-
-        if (requirements.getSuitableForTheseDiets() != null && !requirements.getSuitableForTheseDiets().isEmpty()) {
-            query += "AND COALESCE(dts.name, '') IN (:diets) ";
-            params.put("diets", requirements.getSuitableForTheseDiets()
-                    .stream()
-                    .map(DietDTO::getName)
-                    .toList()
-            );
-        }
-
-        if (requirements.getNutrients() != null && !requirements.getNutrients().isEmpty()) {
-            query += "AND n.id IN (:nutrientIds) ";
-            params.put("nutrientIds",
-                    requirements.getNutrients()
-                            .stream()
-                            .map(NutrientDTO::getId)
-                            .toList()
-            );
-        }
-
-        if (requirements.getDifficulty() != null && !requirements.getDifficulty().isEmpty()) {
-            query += "AND r.difficulty.name = :difficulty ";
-            params.put("difficulty", requirements.getDifficulty());
-        }
-
+    public List<RecipeEntity> findByDynamicQuery(String query, Map<String, Object> params) {
         return find(query, params).list();
     }
 
