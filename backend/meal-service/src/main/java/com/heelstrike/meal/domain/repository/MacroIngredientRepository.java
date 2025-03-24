7c98f9e73b4988df5b4c4e8c0ed32a88239a2a35
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-24 21:38:10 +0000

    feat(meal-service): Add MacroIngredientRepository

---
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/repository/MacroIngredientRepository.java
@@ -0,0 +1,14 @@
+package com.heelstrike.meal.domain.repository;
+
+import com.heelstrike.meal.domain.entity.MacroIngredientEntity;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.Optional;
+
+@ApplicationScoped
+public class MacroIngredientRepository implements PanacheRepository<MacroIngredientEntity> {
+        public Optional<MacroIngredientEntity> findByName(String name) {
+            return find("name", name).firstResultOptional();
+    }
+}
