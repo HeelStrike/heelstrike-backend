9ef17d2a37958ec064edcaa583baa42c4bed08c6
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-24 21:26:54 +0000

    feat(meal-service): Add DifficultyRepository
    	- Add DifficultyRepository class
    		- Add findByName method

--- backend/meal-service/src/main/java/com/heelstrike/meal/domain/repository/DifficultyRepository.java
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/repository/DifficultyRepository.java
@@ -2,6 +2,13 @@ package com.heelstrike.meal.domain.repository;
 
 import com.heelstrike.meal.domain.entity.DifficultyEntity;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.Optional;
+
+@ApplicationScoped
 public class DifficultyRepository implements PanacheRepository<DifficultyEntity> {
+    public Optional<DifficultyEntity> findByName(String name) {
+        return find("name", name).firstResultOptional();
+    }
 }
