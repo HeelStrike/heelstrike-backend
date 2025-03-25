79ada2b8646686cdd816465f386934b31248527e
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-25 22:24:55 +0000

    feat(meal-service): MicroIngredientRepository, add findByDynamicQuery

--- backend/meal-service/src/main/java/com/heelstrike/meal/domain/repository/MicroIngredientRepository.java
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/repository/MicroIngredientRepository.java
@@ -2,9 +2,12 @@ package com.heelstrike.meal.domain.repository;
 
 import com.heelstrike.meal.domain.entity.MacroIngredientEntity;
 import com.heelstrike.meal.domain.entity.MicroIngredientEntity;
+import com.heelstrike.meal.domain.entity.RecipeEntity;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @ApplicationScoped
@@ -13,4 +16,9 @@ public class MicroIngredientRepository implements PanacheRepository<MicroIngredi
     public Optional<MicroIngredientEntity> findByName(String name) {
         return find("name", name).firstResultOptional();
     }
+
+    public List<MicroIngredientEntity> findByDynamicQuery(String query, Map<String, Object> params) {
+        return find(query, params).list();
+    }
+
 }
