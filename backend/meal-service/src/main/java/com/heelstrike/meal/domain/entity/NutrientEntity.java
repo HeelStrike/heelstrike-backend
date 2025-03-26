069e80e007bb6d164df96912a926cf74b9c7ef6d
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-26 19:13:04 +0000

    fix(meal-service): NutrientEntity, convert primitives to boxed

--- backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/NutrientEntity.java
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/NutrientEntity.java
@@ -16,7 +16,7 @@ public class NutrientEntity {
             updatable = false,
             nullable = false
     )
-    private long id;
+    private Long id;
 
     @Column(name = "name")
     private String name;
@@ -30,11 +30,11 @@ public class NutrientEntity {
     @JsonBackReference
     private Set<MicroIngredientEntity> microIngredients;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
