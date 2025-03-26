acac058cc04a937cb410885ac2c2553af1800522
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-26 19:12:06 +0000

    fix(meal-service): DietEntity, convert primitives to boxed

--- backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/DietEntity.java
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/DietEntity.java
@@ -12,12 +12,12 @@ public class DietEntity {
             updatable = false,
             nullable = false
     )
-    private long id;
+    private Long id;
 
     @Column(name = "name")
     private String name;
 
-    public long getId() {
+    public Long getId() {
         return this.id;
     }
 
