70a9278255ed03c470f08399d8467e425adb3102
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-31 18:12:33 +0100

    feat(meal-service): Add schema descriptions

--- backend/meal-service/src/main/java/com/heelstrike/meal/domain/dto/RecipeDTO.java
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/dto/RecipeDTO.java
@@ -1,21 +1,52 @@
 package com.heelstrike.meal.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import java.util.List;
 import java.util.Set;
 
+@Schema(description = "Recipe Data Transfer Object")
 public class RecipeDTO {
+
+    @Schema(examples = "3")
     private Long id;
+
+    @Schema(examples = "Roast Beef with Garlic Roast Potatoes and Honey Roasted Carrots with Onion Gravy ")
     private String title;
+
+    @Schema(examples = "A hearty and traditional Sunday roast dish, perfect for family gatherings.",
+            description = "Short description of the recipe")
     private String description;
+
+    @Schema(examples = "1. Preheat oven to 200°C.\n2. Season beef and roast for 1 hour.\n3. Toss potatoes in garlic and olive oil, roast until golden.\n4. Glaze carrots with honey and roast.\n5. Make onion gravy in a pan.\n6. Serve all together.",
+            description = "Step-by-step cooking instructions")
     private String cookingInstructions;
+
+    @Schema(examples = "1h 30m", description = "Total cooking time")
     private String cookingTime;
+
+    @Schema(examples = "30m", description = "Preparation time before cooking")
     private String preparationTime;
+
+    @Schema(examples = "4", description = "Number of servings")
     private Integer serves;
+
+    @Schema(examples = "Intermediate", description = "Difficulty level of the recipe")
     private String difficulty;
+
+    @Schema(description = "A major ingredient in a recipe, typically a combination of micro-ingredients (e.g., Tomato Pasata, Pesto, Chunky Chips etc...).")
     private Set<MacroIngredientDTO> macroIngredients;
+
+    @Schema(description = "List of micro ingredients such as spices or condiments")
     private Set<MicroIngredientDTO> microIngredients;
+
+    @Schema(description = "Allergens present in the recipe")
     private Set<AllergenDTO> allergens;
+
+    @Schema(description = "Diets this recipe is suitable for (e.g., Gluten Free, Keto)")
     private Set<DietDTO> dietarySuitability;
+
 
     public Long getId() {
         return this.id;
