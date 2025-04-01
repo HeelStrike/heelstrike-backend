6cc2577ce4d1b1e4a220da259c43355668015d7a
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-01 14:33:54 +0100

    feat(meal-service): Add RecipeTypeEntity

---
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/entity/RecipeTypeEntity.java
@@ -0,0 +1,35 @@
+package com.heelstrike.meal.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "recipe_type")
+public class RecipeTypeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(
+            name = "id",
+            updatable = false,
+            nullable = false
+    )
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
