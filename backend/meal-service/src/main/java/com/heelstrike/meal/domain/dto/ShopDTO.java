0f038ea291d9e6df445139c0a4cc143802024423
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-08 12:25:52 +0100

    feat(meal-service): Add ShopDTO

---
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/dto/ShopDTO.java
@@ -0,0 +1,23 @@
+package com.heelstrike.meal.domain.dto;
+
+public class ShopDTO {
+
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
