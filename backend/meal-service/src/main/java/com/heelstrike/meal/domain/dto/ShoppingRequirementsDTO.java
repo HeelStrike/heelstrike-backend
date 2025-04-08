e07480c2875d300991cff1c5fce26b603033d8d8
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-08 12:38:37 +0100

    feat(meal-service): Add ShoppingRequirementsDTO

--- backend/meal-service/src/main/java/com/heelstrike/meal/domain/dto/ShoppingRequirementsDTO.java
+++ backend/meal-service/src/main/java/com/heelstrike/meal/domain/dto/ShoppingRequirementsDTO.java
@@ -1,5 +1,62 @@
 package com.heelstrike.meal.domain.dto;
 
+import java.util.List;
+
 public class ShoppingRequirementsDTO {
+
+    private Float calories;
+    private List<String> keywords;
+    private List<NutrientDTO> nutrientsRequired;
+    private List<AllergenDTO> allergensToAvoid;
+    private List<ShopDTO> shopPreferences;
+    private List<MicroIngredientDTO> microIngredientsToAvoid;
+
+    public Float getCalories() {
+        return calories;
+    }
+
+    public void setCalories(Float calories) {
+        this.calories = calories;
+    }
+
+    public List<String> getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    public List<NutrientDTO> getNutrientsRequired() {
+        return nutrientsRequired;
+    }
+
+    public void setNutrientsRequired(List<NutrientDTO> nutrientsRequired) {
+        this.nutrientsRequired = nutrientsRequired;
+    }
+
+    public List<AllergenDTO> getAllergensToAvoid() {
+        return allergensToAvoid;
+    }
+
+    public void setAllergensToAvoid(List<AllergenDTO> allergensToAvoid) {
+        this.allergensToAvoid = allergensToAvoid;
+    }
+
+    public List<ShopDTO> getShopPreferences() {
+        return shopPreferences;
+    }
+
+    public void setShopPreferences(List<ShopDTO> shopPreferences) {
+        this.shopPreferences = shopPreferences;
+    }
+
+    public List<MicroIngredientDTO> getMicroIngredientsToAvoid() {
+        return microIngredientsToAvoid;
+    }
+
+    public void setMicroIngredientsToAvoid(List<MicroIngredientDTO> microIngredientsToAvoid) {
+        this.microIngredientsToAvoid = microIngredientsToAvoid;
+    }
 }
 
