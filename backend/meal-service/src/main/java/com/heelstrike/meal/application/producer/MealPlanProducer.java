08a25534ed67c4a5d42bfef50d5be369767505c1
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-24 23:04:57 +0100

    feat(meal-service): Add produceEligibleRecipes method

--- backend/meal-service/src/main/java/com/heelstrike/meal/application/producer/MealPlanProducer.java
+++ backend/meal-service/src/main/java/com/heelstrike/meal/application/producer/MealPlanProducer.java
@@ -1,4 +1,23 @@
 package com.heelstrike.meal.application.producer;
 
+import com.heelstrike.meal.application.service.RecipeService;
+import com.heelstrike.meal.domain.dto.RecipeRequirementsDTO;
+import com.heelstrike.meal.domain.entity.RecipeEntity;
+import jakarta.inject.Inject;
+
+import java.util.List;
+
 public class MealPlanProducer {
+
+    @Inject
+    RecipeService recipeService;
+
+    /**
+     * <h1>Eligible Recipes Producer</h1>
+     * Produces List of eligible recipes
+     * @return - List of RecipeEntity objects based upon requirements.
+     */
+    public List<RecipeEntity> produceEligibleRecipes(RecipeRequirementsDTO recipeRequirements) {
+        return recipeService.getRecipesByRequirements(recipeRequirements);
+    }
 }
